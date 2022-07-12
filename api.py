1d3e24bd34cd214c7d8e6908715c82df1b861fa2
sebavergarasassi <106410561+sebavergarasassi@users.noreply.github.com>
2022-07-12 00:19:35 -0300

    Update api.py

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from typing import Optional
 import  random 
 
-cars_api=FastAPI(title="API de  para el Control de stock de Autos",description="API de registro de autos, nuevos, usados y aleatorios")
+cars_api=FastAPI(title="API para el Control de stock de Autos",description="API de registro de autos, nuevos, usados y aleatorios")
 
 CARS=[]
 
@@ -20,19 +20,21 @@ class used_car(car_class):
 
 @cars_api.get("/")
 def home_root():
-    return {"Message":"This is the root, please visit 127.0.0.1:8000/docs"}
+    return {"Message":"This is the root, please visit /docs"}
 
 @cars_api.post("/newcar")
 def new_incoming(car:car_class):
+    "register number will be reassigned by the api "
     car.register_num=random.randrange(0,999999)
     CARS.append(car)
-    return {"Message":"New car "+car.mark +"has been recived"}
+    return {"Message":"New car "+car.mark +" has been received and register number is "+str(car.register_num)}
 
 @cars_api.post("/usedcar")
 def used_incomming(car:used_car):
+    "register number will be reassigned by the api "
     car.register_num=random.randrange(0,999999)
     CARS.append(car)
-    return {"Message":"Used car has been recived"}
+    return {"Message":"Used car "+car.mark +" has been received and register number is "+str(car.register_num)}
 
 @cars_api.post("/randomcar")
 def CREATE_A_RANDOM_CAR():
@@ -42,7 +44,7 @@ def CREATE_A_RANDOM_CAR():
     car=car_class(mark=random.choice(mark_list),manufacture_year=myea,color=random.choice(color_list))
     car.register_num=random.randrange(0,999999)
     CARS.append(car)
-    return {"Message":"Random car has been recived"}
+    return {"Message":"Random car whit register number: "+str(car.register_num)+" has been received"}
     
 @cars_api.get("/cars")
 def stock():
@@ -70,26 +72,32 @@ async def delete_car(register_to_erase:int):
                 CARS.pop(i)
                 return {"Message":"The Car whit register number "+str(register_to_erase)+" has been deleted"}
             i=i+1
-            #condicion de registro no encontrado
-            if i+1==initial_len:
-                return {"Message":"The Car whit register number "+str(register_to_erase)+" was not found"}
-    #condicion de lista vacia        
-    return {"Message":"The Car whit register number "+str(register_to_erase)+" was not found, try to enter a car"}
+    #condicion de registro no encontrado       
+    return {"Message":"The Car whit register number "+str(register_to_erase)+" was not found, try to enter other register number"}
         
 
 @cars_api.put("/cars/{register_to_update}")
 def car_data_update(register_to_update:int,update_car:car_class):
-    i=0
-    for car in CARS:
-        if int(car.__getattribute__("register_num"))==register_to_update:
-            CARS.pop(i)
-        i=i+1
-    update_car.register_num=register_to_update
-    CARS.append(update_car)
-    return {"Message":"The Car whit register number "+str(register_to_update)+" has been updated"}
-    
+    initial_len=len(CARS)
+    #condicion de lista CARS no vacia
+    if initial_len>0:
+        i=0
+        for car in CARS:
+            #condicion de registro encontrado
+            if int(car.__getattribute__("register_num"))==register_to_update:
+                CARS.pop(i)
+                update_car.register_num=register_to_update
+                CARS.append(update_car)
+                return {"Message":"The Car whit register number "+str(register_to_update)+" has been updated"}
+            i=i+1
+    #condicion de registro no encontrado       
+    return {"Message":"The Car whit register number "+str(register_to_update)+" was not found, please check the  number of register"}
 
-    
+
+
+
+
+
 
 
 
